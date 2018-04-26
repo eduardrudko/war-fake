3a37174436954148a2ff1473275d7209845d7441
Eduard Rudko <eduard.rudko@qatestlab.net>
2018-04-26 22:19:39 +0300

    ADD: new abstract Squad class

--- src/warfake/war/battlefield/Squad.java
+++ src/warfake/war/battlefield/Squad.java
@@ -1,15 +1,25 @@
 package warfake.war.battlefield;
 
-import java.util.ArrayList;
+import java.util.Random;
 
-import warfake.war.fraction.Aliance;
-import warfake.war.fraction.Horde;
+public abstract class Squad {
+	private static final short NUMBER_OF_MAGES = 1;
+	private static final short NUMBER_OF_ARCHERS = 3;
+	private static final short NUMBER_OF_WARRIORS = 4;
 
-public class Squad implements Aliance, Horde {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	protected static short getNumberOfMages() {
+		return NUMBER_OF_MAGES;
 	}
 
+	protected static short getNumberOfArchers() {
+		return NUMBER_OF_ARCHERS;
+	}
+
+	protected static short getNumberOfWarriors() {
+		return NUMBER_OF_WARRIORS;
+	}
+
+	public static void main(String[] args) {
+
+	}
 }

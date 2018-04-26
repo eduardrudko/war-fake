51a7f420aa5bd704adaa0ad260c2ad4484766c02
Eduard Rudko <eduard.rudko@qatestlab.net>
2018-04-26 22:17:48 +0300

    ADD: new abstract Character class

---
+++ src/warfake/war/race/Character.java
@@ -0,0 +1,15 @@
+package warfake.war.race;
+
+public abstract class Character {
+	private short HP = 100;
+	
+	public short showHealth() {
+		return HP;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

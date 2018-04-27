3b889f20f97c40929f8bb3a3aab51ce74d6d1e0d
Eduard Rudko <eduard.rudko@qatestlab.net>
2018-04-27 20:17:21 +0300

    ADD: max health and max health methods for abstract class Person

--- src/warfake/war/race/Person.java
+++ src/warfake/war/race/Person.java
@@ -1,15 +1,27 @@
 package warfake.war.race;
 
-public abstract class Character {
-	private short HP = 100;
-	
-	public short showHealth() {
+public abstract class Person {
+	private int HP = 100;
+	private static final int MAX_HP = 100;
+	private int accuracy = 100;
+
+	public int getHealth() {
 		return HP;
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public int getMaxHealth() {
+		return MAX_HP;
 	}
 
+	public void setHealth(int health) {
+		this.HP = health;
+	}
+
+	public int getAccuracy() {
+		return accuracy;
+	}
+
+	public void setAccuracy(int accuracy) {
+		this.accuracy = accuracy;
+	}
 }

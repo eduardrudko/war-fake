d09bf9cf1e6f182c157e840917d63f09ce10ff3c
Eduard Rudko <eduard.rudko@qatestlab.net>
2018-04-27 21:53:38 +0300

    UPD: Person class

--- src/warfake/war/race/Person.java
+++ src/warfake/war/race/Person.java
@@ -1,19 +1,19 @@
 package warfake.war.race;
 
 public abstract class Person {
-	private int HP = 100;
-	private static final int MAX_HP = 100;
+	private float HP = 100;
+	private static final float MAX_HP = 100;
 	private int accuracy = 100;
 
-	public int getHealth() {
+	public float getHealth() {
 		return HP;
 	}
 
-	public int getMaxHealth() {
+	public float getMaxHealth() {
 		return MAX_HP;
 	}
 
-	public void setHealth(int health) {
+	public void setHealth(float health) {
 		this.HP = health;
 	}
 
